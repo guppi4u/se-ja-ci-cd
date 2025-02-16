9f9e60eee88c808effbaff2be98f25ac06774a25
guppi <guppi4u@outlook.com>
2025-02-16 12:28:11 +1100

    First Hello World commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+W
+'src/main/java/com/guppi/HelloWorld.java,d/f/dff6adb84bb1a27c7dc07e09ff918c907e5206dc
---
+++ src/main/java/com/guppi/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.guppi;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
